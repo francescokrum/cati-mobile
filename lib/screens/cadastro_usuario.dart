cf43c7fba54a3f80c9d9cc0113fbbbcee9b12f53
francescokrum <francescopk19@gmail.com>
2024-07-11 15:40:58 -0300

    (feat): tela de cadastro de Usuario

---
+++ lib/screens/cadastro_usuario.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+import '../database/usuario_dao.dart';
+import 'login_screen.dart'; // Importe o arquivo onde está definida a tela de login
+import '../model/usuario.dart'; // Importe o modelo de usuário
+import '../database/usuario_dao.dart'; // Importe a classe UsuarioDAO onde está definida a lista de usuários
+
+class CadastroUsuario extends StatefulWidget {
+  const CadastroUsuario({Key? key}) : super(key: key);
+
+  @override
+  _CadastroUsuarioState createState() => _CadastroUsuarioState();
+}
+
+class _CadastroUsuarioState extends State<CadastroUsuario> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _cpfController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _loginController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
+  final TextEditingController _cnpjEmpresaController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Central de Atendimento de TI',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.purple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 100, left: 16, right: 16),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.disabled,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20),
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'CADASTRO DE USUÁRIO',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  controller: _nomeController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nome',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _cpfController,
+                  decoration: InputDecoration(
+                    labelText: 'CPF',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _loginController,
+                  decoration: InputDecoration(
+                    labelText: 'Login',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _senhaController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _cnpjEmpresaController,
+                  decoration: InputDecoration(
+                    labelText: 'CNPJ da Empresa',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Campo obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Todos os campos são válidos, então podemos cadastrar o usuário
+                      final String nome = _nomeController.text.trim();
+                      final String cpf = _cpfController.text.trim();
+                      final String email = _emailController.text.trim();
+                      final String login = _loginController.text.trim();
+                      final String senha = _senhaController.text.trim();
+                      final String cnpjEmpresa = _cnpjEmpresaController.text.trim();
+
+                      // Criar um novo objeto Usuario
+                      Usuario usuario = Usuario(
+                        nome: nome,
+                        cpf: cpf,
+                        email: email,
+                        login: login,
+                        senha: senha,
+                        cnpjEmpresa: cnpjEmpresa,
+                      );
+
+                      // Adicionar o usuário à lista usando UsuarioDAO
+                      UsuarioDAO.adicionar(usuario);
+
+                      // Mostrar mensagem de sucesso
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Usuário cadastrado com sucesso!'),
+                        ),
+                      );
+
+                      // Limpar os campos do formulário após o cadastro
+                      _nomeController.clear();
+                      _cpfController.clear();
+                      _emailController.clear();
+                      _loginController.clear();
+                      _senhaController.clear();
+                      _cnpjEmpresaController.clear();
+                    } else {
+                      // Exibir mensagem de erro se algum campo não estiver preenchido corretamente
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Por favor, preencha todos os campos obrigatórios.'),
+                        ),
+                      );
+                    }
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.purple),
+                  ),
+                  child: const Text(
+                    'CADASTRAR',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => const LoginScreen()),
+                    );
+                  },
+                  child: const Text(
+                    'Já tem uma conta? Faça o Login',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _nomeController.dispose();
+    _cpfController.dispose();
+    _emailController.dispose();
+    _loginController.dispose();
+    _senhaController.dispose();
+    _cnpjEmpresaController.dispose();
+    super.dispose();
+  }
+}
